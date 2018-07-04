d00f77579f2aebc061700707158781af973ed832
982571 <982571@CS_V45-308.cs.mum.edu>
2018-07-04 16:49:12 -0500

    Best friends

--- ui/Login.java
+++ ui/Login.java
@@ -27,7 +27,7 @@ public class Login extends Application {
 		primaryStage.setTitle("Login Form");
 // moad last chance
 		// nfnfnfnfn
-		// Maram
+		// Maram and jana
 		// you can communicate with the components using id's
 		TextField usertf = (TextField) root.lookup("#usertf");
 		PasswordField passwordField = (PasswordField) root.lookup("#passwordField");
